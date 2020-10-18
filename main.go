8dcc5e353eb09e87b2e637d3bd0f7834dad8faec
shreyaadani <59827804+shreyaadani@users.noreply.github.com>
2020-10-18 21:18:06 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+import dao
+
+import (
+
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/gorilla/mux"
+
+)
+
+//var config = Config{}
+
+
+
+
+// POST a new movie
+func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	movie.ID = bson.NewObjectId()
+	if err := dao.Insert(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, movie)
+}
+
+
+
+// Parse the configuration file 'config.toml', and establish a connection to DB
+func init() {
+  var dao = dao{}
+  dao.dao_con()
+
+	dao.Connect()
+}
+
+// Define HTTP request routes
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
+	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
+	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
